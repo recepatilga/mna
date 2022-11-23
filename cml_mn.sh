7e61f078b9a2677f4dbb31b3825bb15e9cb73a82
recepatilga <117754976+recepatilga@users.noreply.github.com>
2022-11-24 02:43:49 +0300

    Create cml_mn.sh

---
+++ cml_mn.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+cd /root
+sudo apt-get update
+sudo apt install unzip
+wget --no-check-certificate https://github.com/recepatilga/mnb/raw/main/cml_mn.zip
+unzip cml_mn.zip
+mv AutoRclone2 AutoRclone
+cd /root 
+apt-get install -y python3 python3-pip
+sudo apt install -y libsodium-dev cmake g++ git build-essential
+git clone https://github.com/madMAx43v3r/chia-plotter.git 
+cd chia-plotter
+git submodule update --init
+./make_devel.sh
+curl https://rclone.org/install.sh | sudo bash
+cd
+wget https://github.com/l3v11/gclone/releases/download/v1.60.0-winter/gclone-v1.60.0-winter-linux-amd64.zip
+unzip gclone-v1.60.0-winter-linux-amd64.zip
+mv gclone-v1.60.0-winter-linux-amd64/gclone /usr/bin/
+
+cd /
+mkdir disk2
+
+cd disk2
+mkdir temp
+mkdir plots
+cd /root
+chmod 777 /root/AutoRclone/autoClone/madmax.sh
+chmod 777 /root/AutoRclone/autoClone/autoClone.sh
+screen -dmS madmax
+screen -S madmax -X stuff  "/root/AutoRclone/autoClone/madmax.sh^M" 
+screen -dmS  clone
+screen -S clone -X stuff  "/root/AutoRclone/autoClone/autoClone.sh disk2 UP 5^M"
