f52a868ebccaf1e0499f94d5c41553985985eb5e
recepatilga <117754976+recepatilga@users.noreply.github.com>
2022-12-03 12:01:18 +0300

    Update mdk_mn.sh

--- mdk_mn.sh
+++ mdk_mn.sh
@@ -28,4 +28,4 @@ cd /root
 chmod 777 /root/AutoRclone/autoClone/madmax.sh
 chmod 777 /root/AutoRclone/autoClone/autoClone.sh
 screen -dmS madmax
-screen -S madmax -X stuff  "/root/AutoRclone/autoClone/madmax.sh UPM 2 300 6^M"
+screen -S madmax -X stuff  "/root/AutoRclone/autoClone/madmax.sh UPM 0 200 6^M"
