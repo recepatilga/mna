91c23b0810f063e19b9692b062364cca52cbeff5
recepatilga <117754976+recepatilga@users.noreply.github.com>
2022-12-12 21:36:35 +0300

    Update hjhg.sh

--- hjhg.sh
+++ hjhg.sh
@@ -5,7 +5,7 @@
 wget https://www.otohits.net/dl/OtohitsApp_5068_linux_portable.tar.gz > /dev/null
 mkdir OtohitsApp
 cd OtohitsApp
-echo "/login:ef8ee361-eefc-4190-84b0-892f1add8c76 " > otohits.ini
+echo "/login:ef8ee361-eefc-4190-84b0-892f1add8c76" > otohits.ini
 echo "/autoupdate" >> otohits.ini
 tar -xzf ../OtohitsApp_5068_linux_portable.tar.gz
 
