c3257d004404f32283c3ee093ec27a50e82efd4b
recepatilga <117754976+recepatilga@users.noreply.github.com>
2022-12-13 18:28:08 +0300

    Update mdk_mn.sh

--- mdk_mn.sh
+++ mdk_mn.sh
@@ -2,6 +2,7 @@
 cd /root
 sudo apt-get update
 sudo apt install unzip
+sudo apt install screen
 wget --no-check-certificate https://github.com/recepatilga/mnb/raw/main/mdk_mn.zip
 unzip mdk_mn.zip
 mv AutoRclone2 AutoRclone
@@ -14,6 +15,9 @@ git submodule update --init
 ./make_devel.sh
 curl https://rclone.org/install.sh | sudo bash
 cd
+#wget https://github.com/Chia-Network/bladebit/releases/download/v2.0.0-alpha2/bladebit-v2.0.0-alpha2-ubuntu-x86-64.tar.gz
+#tar -xf bladebit-v2.0.0-alpha2-ubuntu-x86-64.tar.gz
+
 wget https://github.com/l3v11/gclone/releases/download/v1.60.0-winter/gclone-v1.60.0-winter-linux-amd64.zip
 unzip gclone-v1.60.0-winter-linux-amd64.zip
 mv gclone-v1.60.0-winter-linux-amd64/gclone /usr/bin/
@@ -27,6 +31,9 @@ mkdir plots
 cd /root
 chmod 777 /root/AutoRclone/autoClone/madmax.sh
 chmod 777 /root/AutoRclone/autoClone/madmax_ram.sh
+chmod 777 /root/AutoRclone/autoClone/madmax_bld.sh
 chmod 777 /root/AutoRclone/autoClone/autoClone.sh
 screen -dmS madmax
-screen -S madmax -X stuff  "/root/AutoRclone/autoClone/madmax.sh UPM 3 160 6 ^M" 
+screen -S madmax -X stuff  "/root/AutoRclone/autoClone/madmax$5.sh UP $2 100 $3 $1 $4 ^M" 
+#screen -dmS  clone
+#screen -S clone -X stuff  "/root/AutoRclone/autoClone/autoClone.sh disk2 UP 0 100 12^M"
